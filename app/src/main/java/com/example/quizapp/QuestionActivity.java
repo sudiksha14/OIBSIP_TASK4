f0e8a0da58f7dac7bd7602dc1cc296034e028b4e
sudiksha14 <sudikshagunjkar@gmail.com>
2022-07-20 16:48:36 +0530

    QUIZ APP

---
+++ app/src/main/java/com/example/quizapp/QuestionActivity.java
@@ -0,0 +1,130 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuestionActivity extends AppCompatActivity {
+    TextView tv;
+    Button submitbutton, quitbutton;
+    RadioGroup radio_g;
+    RadioButton rb1,rb2,rb3,rb4;
+
+    String questions[] = {
+            "Who developed Python Programming Language?",
+            "Which type of Programming does Python support?",
+            "Is Python case sensitive when dealing with identifiers?",
+            "Which of the following is the correct extension of the Python file?",
+            "Is Python code compiled or interpreted?",
+            "All keywords in Python are in _________",
+            "Which of the following is used to define a block of code in Python language?",
+            "Which keyword is used for function in Python language?",
+            "Which of the following character is used to give single-line comments in Python?",
+            "Python supports the creation of anonymous functions at runtime, using a construct called __________"
+    };
+    String answers[] = {"Guido van Rossum","all of the mentioned","no",".py","Python code is neither compiled nor interpreted","None of the mentioned","Indentation","Def","#","lambda"};
+    String opt[] = {
+            "Wick van Rossum","Rasmus Lerdorf","Guido van Rossum","Niene Stom",
+            "object-oriented programming","structured programming","functional programming","all of the mentioned",
+            "no","yes"," machine dependent","none of the mentioned",
+            ".python",".pl",".py",".p",
+            "Python code is both compiled and interpreted","Python code is neither compiled nor interpreted","Python code is only compiled","Python code is only interpreted",
+            "Capitalized","lower case","UPPER CASE","None of the mentioned",
+            "Indentation","Key","Brackets","All of the mentioned",
+            "Function","Def","Fun","Define",
+            "//","#","!","/*",
+            "pi","anonymous","lambda","none of the mentioned"
+    };
+    int flag=0;
+    public static int marks=0,correct=0,wrong=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+        final TextView score = (TextView)findViewById(R.id.textView4);
+        TextView textView=(TextView)findViewById(R.id.DispName);
+        Intent intent = getIntent();
+        String name= intent.getStringExtra("myname");
+
+        if (name.trim().equals(""))
+            textView.setText("Hello User");
+        else
+            textView.setText("Hello " + name);
+
+        submitbutton=(Button)findViewById(R.id.button3);
+        quitbutton=(Button)findViewById(R.id.buttonquit);
+        tv=(TextView) findViewById(R.id.tvque);
+
+        radio_g=(RadioGroup)findViewById(R.id.answersgrp);
+        rb1=(RadioButton)findViewById(R.id.radioButton);
+        rb2=(RadioButton)findViewById(R.id.radioButton2);
+        rb3=(RadioButton)findViewById(R.id.radioButton3);
+        rb4=(RadioButton)findViewById(R.id.radioButton4);
+        tv.setText(questions[flag]);
+        rb1.setText(opt[0]);
+        rb2.setText(opt[1]);
+        rb3.setText(opt[2]);
+        rb4.setText(opt[3]);
+        submitbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(radio_g.getCheckedRadioButtonId()==-1)
+                {
+                    Toast.makeText(getApplicationContext(), "Please select one choice", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                RadioButton uans = (RadioButton) findViewById(radio_g.getCheckedRadioButtonId());
+                String ansText = uans.getText().toString();
+//                Toast.makeText(getApplicationContext(), ansText, Toast.LENGTH_SHORT).show();
+                if(ansText.equals(answers[flag])) {
+                    correct++;
+                    Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    wrong++;
+                    Toast.makeText(getApplicationContext(), "Wrong", Toast.LENGTH_SHORT).show();
+                }
+
+                flag++;
+
+                if (score != null)
+                    score.setText(""+correct);
+
+                if(flag<questions.length)
+                {
+                    tv.setText(questions[flag]);
+                    rb1.setText(opt[flag*4]);
+                    rb2.setText(opt[flag*4 +1]);
+                    rb3.setText(opt[flag*4 +2]);
+                    rb4.setText(opt[flag*4 +3]);
+                }
+                else
+                {
+                    marks=correct;
+                    Intent in = new Intent(getApplicationContext(),ResultActivity.class);
+                    startActivity(in);
+                }
+                radio_g.clearCheck();
+            }
+        });
+
+        quitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ResultActivity.class);
+                startActivity(intent);
+            }
+        });
+            }
+
+            }
+
